a1f7c22bdbd7394e24d13b020d70042626fb2311
Jeffrey Lau <jeffrey.lau@ribose.com>
2023-11-22 16:22:44 +0800

    style: Run yarn lint:fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,39 @@
 
-        import React from 'react'
-        import ReactDOM from 'react-dom'
-        import { AppContainer } from 'react-hot-loader'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
-        // Your top level component
-        import App from './App'
+// Your top level component
+import App from './App'
 
-        // Export your top level component as JSX (for static rendering)
-        export default App
+// Export your top level component as JSX (for static rendering)
+export default App
 
-        // Render your app
-        if (typeof document !== 'undefined') {
-          const target = document.getElementById('root')
+// Render your app
+if (typeof document !== 'undefined') {
+  const target = document.getElementById('root')
 
-          const renderMethod = target.hasChildNodes()
+  const renderMethod = target.hasChildNodes()
             ? ReactDOM.hydrate
             : ReactDOM.render
 
-          const render = (Comp: Function) => {
-            renderMethod(
-              <AppContainer>
-                <Comp />
-              </AppContainer>,
-              target
-            )
-          }
+  const render = (Comp: Function) => {
+    renderMethod(
+      <AppContainer>
+        <Comp />
+      </AppContainer>,
+      target
+    )
+  }
 
-          // Render!
-          render(App)
+  // Render!
+  render(App)
 
-          // Hot Module Replacement
-          if (module && module.hot) {
-            module.hot.accept('./App', () => {
-              render(App)
-            })
-          }
-        }
+  // Hot Module Replacement
+  if (module && module.hot) {
+    module.hot.accept('./App', () => {
+      render(App)
+    })
+  }
+}
       
